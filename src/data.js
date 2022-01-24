87d230deefc6adade46dbeb51c97149c8bbdc142
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2022-01-24 17:15:37 -0500

    add: Added cards and card css rules to display mock browsers as cards. Also implemented data file that contains product information

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        id:1,
+        img: "https://www.kindpng.com/picc/m/7-70057_cartoon-tree-png-clip-art-tree-clipart-transparent.png",
+        link: "https://group-tree.herokuapp.com"
+    },
+    {
+        id:2,
+        img: "https://www.kindpng.com/picc/m/7-70057_cartoon-tree-png-clip-art-tree-clipart-transparent.png",
+        link: "https://group-tree.herokuapp.com"
+    },
+    {
+        id:3,
+        img: "https://www.kindpng.com/picc/m/7-70057_cartoon-tree-png-clip-art-tree-clipart-transparent.png",
+        link: "https://group-tree.herokuapp.com"
+    },
+    {
+        id:4,
+        img: "https://www.kindpng.com/picc/m/7-70057_cartoon-tree-png-clip-art-tree-clipart-transparent.png",
+        link: "https://group-tree.herokuapp.com"
+    },
+    {
+        id:5,
+        img: "https://www.kindpng.com/picc/m/7-70057_cartoon-tree-png-clip-art-tree-clipart-transparent.png",
+        link: "https://group-tree.herokuapp.com"
+    },
+    {
+        id:6,
+        img: "https://www.kindpng.com/picc/m/7-70057_cartoon-tree-png-clip-art-tree-clipart-transparent.png",
+        link: "https://group-tree.herokuapp.com"
+    }
+]
